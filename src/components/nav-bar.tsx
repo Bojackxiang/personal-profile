974e058cf075f3913cd9c4a05fe6ae528343c7ba
Alex <alex@q-83.com>
2024-03-17 13:40:25 +1100

    added mobile menu

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 import Container from './helper/container'
 import Link from 'next/link'
 import MainNav from './helper/main-nav'
+import MobileSideBar from './helper/mobile-side-bar'
+
 
 const NavBar = ({
 }) => {
   return (
     <Container>
-      <div className="flex relative px-4 sm:px-6 lg:px-8 h-16 items-center">
+      <div className="flex flex-row-reverse sm:flex-row  relative px-4 sm:px-6 lg:px-8 h-16 items-center">
         <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
           <p className="font-bold text-xl">Alex</p>
         </Link>
         <MainNav />
+        <MobileSideBar />
+
 
       </div>
     </Container>
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,6 @@
 export const MENU_ITEMS = [
-  { id: "menu-item-1", name: "download-cv", link: "", },
-  { id: "menu-item-2", name: "tech-stack", link: "", },
-  { id: "menu-item-4", name: "education", link: "", },
-  { id: "menu-item-3", name: "experience", link: "", }
+  { id: "menu-item-1", name: "Download CV", link: "", },
+  { id: "menu-item-2", name: "Tech Stack", link: "", },
+  { id: "menu-item-4", name: "Education", link: "", },
+  { id: "menu-item-3", name: "Experience", link: "", }
 ]
