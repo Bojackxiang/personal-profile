1db92e4a18cd6bc91949271e122ea9fc81e9bf33
Alex <alex@q-83.com>
2024-03-18 22:51:22 +1100

    added arrow down icon

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -5,6 +5,7 @@ import Container from './helper/container'
 import { BannerAutoWriter } from './helper/banner-auto-writer'
 import BannerPhotoAndDetials from './helper/banner-photo-details'
 import BannerCVDownload from './helper/banner-cv-download'
+import BannerScrollIcon from './helper/banner-scroll-icon'
 
 const Banner = () => {
   return (
@@ -22,6 +23,10 @@ const Banner = () => {
 
       <Container classNames="max-w-4xl mt-10 sm:mt-20">
         <BannerCVDownload />
+      </Container>
+
+      <Container classNames="max-w-4xl mt-10 sm:mt-[120px]">
+        <BannerScrollIcon />
       </Container>
     </div>
 
---
+++ src/components/helper/banner-scroll-icon.tsx
@@ -0,0 +1,14 @@
+
+import { CircleArrowDown } from 'lucide-react'
+
+
+
+const BannerScrollIcon = () => {
+  return (
+    <section className='text-center'>
+      <CircleArrowDown size={80} className='text-white animate-bounce mx-auto' />
+    </section>
+  )
+}
+
+export default BannerScrollIcon
