8ed65aa61ae70d7b5ec27ffa4620c31f738040be
Alex <alex@q-83.com>
2024-03-18 17:39:34 +1100

    Added type writer

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,13 +1,24 @@
+'use client'
+
 import React from 'react'
 import Container from './helper/container'
+import { ReactTyped } from "react-typed";
+import { TYPE_WRITER_LIST } from '@/constant';
 
 const Banner = () => {
   return (
 
-    <div className='min-h-screen pt-14 bg-cover bg-center bg-no-repeat'
+    <div className='min-h-screen pt-14 bg-cover bg-center bg-no-repeat flex flex-col justify-center'
       style={{ backgroundImage: 'url("/banner-background.jpeg")' }}>
-      <Container classNames="max-w-7xl">
-        banner
+      <Container classNames=" max-w-7xl flex flex-col justify-center items-center">
+        <ReactTyped
+          startWhenVisible
+          strings={TYPE_WRITER_LIST}
+          loop
+          className="text-5xl font-bold text-white"
+          typeSpeed={40}
+        />
+
       </Container>
     </div>
 
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,3 +4,10 @@ export const MENU_ITEMS = [
   { id: "menu-item-4", name: "Education", link: "", },
   { id: "menu-item-3", name: "Experience", link: "", }
 ]
+export const TYPE_WRITER_LIST = [
+  "Hi, I'm Alex",
+  "Welcome to my portfolio!",
+  "I'm a full stack developer",
+  "Here you can find my projects and resume",
+  "Let's start the journey!"
+]
