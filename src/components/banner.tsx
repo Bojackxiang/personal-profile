38bbec775985bb8cafff580813e530f02591f79c
Alex <alex@q-83.com>
2024-03-18 22:16:19 +1100

    update banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -2,24 +2,21 @@
 
 import React from 'react'
 import Container from './helper/container'
-import { ReactTyped } from "react-typed";
-import { TYPE_WRITER_LIST } from '@/constant';
+import { BannerAutoWriter } from './helper/banner-auto-writer'
+import BannerPhotoAndDetials from './helper/banner-photo-details'
 
 const Banner = () => {
   return (
 
-    <div className='min-h-screen pt-14 bg-cover bg-center bg-no-repeat flex flex-col justify-center'
+    <div className='min-h-screen pt-14 bg-cover bg-center bg-no-repeat'
       style={{ backgroundImage: 'url("/banner-background.jpeg")' }}>
-      <Container classNames=" max-w-7xl flex flex-col justify-center items-center">
-        <ReactTyped
-          strings={TYPE_WRITER_LIST}
-          loop
-          className="text-white text-xl text-center sm:text-5xl font-bold"
-          typeSpeed={40}
-          backDelay={2000}
-          showCursor
-        />
 
+      <Container classNames="max-w-7xl bg-transparent flex justify-center mt-10 sm:mt-20">
+        <BannerAutoWriter />
+      </Container>
+
+      <Container classNames="max-w-4xl mt-10 sm:mt-20">
+        <BannerPhotoAndDetials />
       </Container>
     </div>
 
---
+++ src/components/helper/banner-photo-details.tsx
@@ -0,0 +1,36 @@
+import { Container } from 'postcss'
+import Image from 'next/image'
+
+const BannerPhotoAndDetials = () => {
+  return (
+    <section className='flex flex-col sm:flex-row justify-between p-2'>
+      <div className='flex-1'>
+        <Image className='rounded-full mx-auto sm:mx-0' src={"/user-avatar.jpg"} width={300} height={300} alt="user avatar" />
+      </div>
+
+      <div className='flex-1'>
+        <h1 className='text-3xl font-bold text-white text-center py-8'>
+          Glad to see you, I am Alex!
+        </h1>
+        <div className='text-center'>
+          <p className='text-white p-5 sm:p-0'>
+            Welcome to my portfolio! I am looking to a Full Stack Position.
+            I have 2+ years of experience.
+            I am currently looking for a job.
+            I have 2+ years of experience.
+          </p>
+
+          <p className='text-white p-5 sm:p-0'>
+            Welcome to my portfolio! I am looking to a Full Stack Position.
+            I have 2+ years of experience.
+            I am currently looking for a job.
+            I have 2+ years of experience.
+          </p>
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default BannerPhotoAndDetials
